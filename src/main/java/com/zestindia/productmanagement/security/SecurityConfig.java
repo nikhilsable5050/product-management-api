6e505a157a4ad161b2e0f1451650f8a2f25a6c25
nikhilsable5050 <nikhilsable5050@gmail.com>
2026-08-31 14:54:58 +0530

    feat: allow refresh token authentication

--- src/main/java/com/zestindia/productmanagement/security/SecurityConfig.java
+++ src/main/java/com/zestindia/productmanagement/security/SecurityConfig.java
@@ -37,7 +37,8 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
                                 "/api/v1/auth/register",
-                                "/api/v1/auth/login"
+                                "/api/v1/auth/login",
+                                "/api/v1/auth/refresh"
                         ).permitAll()
                         .anyRequest().authenticated()
                 )
