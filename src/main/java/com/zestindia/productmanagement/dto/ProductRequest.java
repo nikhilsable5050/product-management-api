cf264be7b17ec0aeb33f09176545432be9270102
nikhilsable5050 <nikhilsable5050@gmail.com>
2026-08-31 15:09:30 +0530

    feat: add product name size validation

--- src/main/java/com/zestindia/productmanagement/dto/ProductRequest.java
+++ src/main/java/com/zestindia/productmanagement/dto/ProductRequest.java
@@ -1,6 +1,7 @@
 package com.zestindia.productmanagement.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,5 +10,6 @@ import lombok.Setter;
 public class ProductRequest {
 
     @NotBlank(message = "Product name is required")
+    @Size(max = 255, message = "Product name must not exceed 255 characters")
     private String productName;
 }
