2e7299d357d6458a6ef8509e82c66023e665f4d6
nikhilsable5050 <nikhilsable5050@gmail.com>
2026-08-31 13:25:56 +0530

    feat: add product item query to repository

--- src/main/java/com/zestindia/productmanagement/repository/ItemRepository.java
+++ src/main/java/com/zestindia/productmanagement/repository/ItemRepository.java
@@ -3,6 +3,9 @@ package com.zestindia.productmanagement.repository;
 import com.zestindia.productmanagement.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ItemRepository extends JpaRepository<Item, Long> {
 
+    List<Item> findByProductId(Long productId);
 }
