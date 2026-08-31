961de481f88add43987eaaabca084a84dcfabfb3
nikhilsable5050 <nikhilsable5050@gmail.com>
2026-08-31 14:45:01 +0530

    feat: add refresh token service

---
+++ src/main/java/com/zestindia/productmanagement/security/RefreshTokenService.java
@@ -0,0 +1,36 @@
+package com.zestindia.productmanagement.security;
+
+import com.zestindia.productmanagement.entity.RefreshToken;
+import com.zestindia.productmanagement.entity.User;
+import com.zestindia.productmanagement.repository.RefreshTokenRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class RefreshTokenService {
+
+    private final RefreshTokenRepository refreshTokenRepository;
+
+    public RefreshToken createRefreshToken(User user) {
+
+        RefreshToken refreshToken = new RefreshToken();
+
+        refreshToken.setToken(UUID.randomUUID().toString());
+        refreshToken.setUser(user);
+        refreshToken.setExpiresAt(LocalDateTime.now().plusDays(7));
+        refreshToken.setRevoked(false);
+
+        return refreshTokenRepository.save(refreshToken);
+    }
+
+    public boolean isValid(RefreshToken refreshToken) {
+
+        return !refreshToken.isRevoked()
+                && refreshToken.getExpiresAt()
+                .isAfter(LocalDateTime.now());
+    }
+}
