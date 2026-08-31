eb46a5f29d8d69a099063b126403349b50901ea7
nikhilsable5050 <nikhilsable5050@gmail.com>
2026-08-31 13:52:30 +0530

    feat: add refresh token entity

---
+++ src/main/java/com/zestindia/productmanagement/entity/RefreshToken.java
@@ -0,0 +1,33 @@
+package com.zestindia.productmanagement.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "refresh_tokens")
+@Getter
+@Setter
+@NoArgsConstructor
+public class RefreshToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String token;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false)
+    private LocalDateTime expiresAt;
+
+    @Column(nullable = false)
+    private boolean revoked;
+}
