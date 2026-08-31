b6cc7ad70e2cb1c15ddb439cb8fb7ea508db2950
nikhilsable5050 <nikhilsable5050@gmail.com>
2026-08-31 13:29:03 +0530

    feat: add standardized error response

---
+++ src/main/java/com/zestindia/productmanagement/exception/ErrorResponse.java
@@ -0,0 +1,15 @@
+package com.zestindia.productmanagement.exception;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+public class ErrorResponse {
+
+    private int status;
+    private String message;
+    private LocalDateTime timestamp;
+}
