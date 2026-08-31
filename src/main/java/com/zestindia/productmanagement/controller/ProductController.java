57a79018903daf71238ea5253d0ca4ca1b6b4ecd
nikhilsable5050 <nikhilsable5050@gmail.com>
2026-08-31 13:03:31 +0530

    Add controller

---
+++ src/main/java/com/zestindia/productmanagement/controller/ProductController.java
@@ -0,0 +1,61 @@
+package com.zestindia.productmanagement.controller;
+
+import com.zestindia.productmanagement.dto.ProductRequest;
+import com.zestindia.productmanagement.dto.ProductResponse;
+import com.zestindia.productmanagement.service.ProductService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/products")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductService productService;
+
+    @PostMapping
+    public ResponseEntity<ProductResponse> createProduct(
+            @Valid @RequestBody ProductRequest request) {
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(productService.createProduct(request));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ProductResponse>> getAllProducts() {
+
+        return ResponseEntity.ok(productService.getAllProducts());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductResponse> getProductById(
+            @PathVariable Long id) {
+
+        return ResponseEntity.ok(productService.getProductById(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ProductResponse> updateProduct(
+            @PathVariable Long id,
+            @Valid @RequestBody ProductRequest request) {
+
+        return ResponseEntity.ok(
+                productService.updateProduct(id, request)
+        );
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProduct(
+            @PathVariable Long id) {
+
+        productService.deleteProduct(id);
+
+        return ResponseEntity.noContent().build();
+    }
+}
