0631e97341d418b4bdb38dd1ca1eff77bd69ed44
nikhilsable5050 <nikhilsable5050@gmail.com>
2026-08-31 13:40:44 +0530

    feat: add item creation service

--- src/main/java/com/zestindia/productmanagement/service/ItemService.java
+++ src/main/java/com/zestindia/productmanagement/service/ItemService.java
@@ -1,8 +1,13 @@
 package com.zestindia.productmanagement.service;
 
+import com.zestindia.productmanagement.dto.ItemRequest;
 import com.zestindia.productmanagement.dto.ItemResponse;
 import com.zestindia.productmanagement.entity.Item;
+import com.zestindia.productmanagement.entity.Product;
+import com.zestindia.productmanagement.exception.ProductNotFoundException;
 import com.zestindia.productmanagement.repository.ItemRepository;
+import com.zestindia.productmanagement.repository.ProductRepository;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +17,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ItemService {
 
+    private final ProductRepository productRepository;
     private final ItemRepository itemRepository;
 
     public List<ItemResponse> getItemsByProductId(Long productId) {
@@ -29,5 +35,21 @@ public class ItemService {
                 .productId(item.getProduct().getId())
                 .quantity(item.getQuantity())
                 .build();
+    }
+
+    public ItemResponse createItem(Long productId, ItemRequest request) {
+
+        Product product = productRepository.findById(productId)
+                .orElseThrow(() ->
+                        new ProductNotFoundException("Product not found"));
+
+        Item item = new Item();
+
+        item.setProduct(product);
+        item.setQuantity(request.getQuantity());
+
+        Item savedItem = itemRepository.save(item);
+
+        return mapToResponse(savedItem);
     }
 }
